23d5e7c2498275315ba8571580670f9a10ebe678
Ani Khalili <anikhalili@anis-macbook-pro.local>
2016-03-21 22:00:14 -0700

    Initial Commit

---
+++ StudyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyApp
+//
+//  Created by Ani Khalili on 3/21/16.
+//  Copyright © 2016 Ani Khalili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
